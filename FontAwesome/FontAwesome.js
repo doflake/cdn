6baaec2abf7ebf8fd4243628a399d56709fde0a3
doflake <1242803739@qq.com>
2021-11-19 16:08:55 +0800

    update

---
+++ FontAwesome/FontAwesome.js
@@ -0,0 +1,4 @@
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/doflake/cdn/FontAwesome/css/free.min.css">');
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/doflake/cdn/FontAwesome/css/free-v4-font-face.min.css">');
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/doflake/cdn/FontAwesome/css/free-v4-shims.min.css">');
+
